74ff30cc7dee86826ed22c06fe4e17d6aca9338b
Lucas Leonardo <luccasleonardo8@gmail.com>
2024-07-02 20:42:09 -0300

    tcc

---
+++ home/script.js
@@ -0,0 +1,20 @@
+const video = document.getElementById("myVideo");
+
+const btn = document.getElementById("myBtn");
+
+function myFunction() {
+
+    if (video.paused)
+
+    {
+        video.onplay();
+        btn.innerHTML = "Pause";
+    }
+
+    else
+
+    {
+        video.pause();
+        btn.innerHTML = "play";
+    }
+}
